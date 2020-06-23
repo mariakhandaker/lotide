ee2f3330977cf1e12767300f521065562a5bc280
Maria Khandaker <mariakhandaker@gmail.com>
2020-06-23 01:49:50 +0000

    assert equal function, used in place of console.assert()

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(" ✅ Assertion Passed ✅: " + actual + " === " + expected);
+  } else {
+    console.log(" 🛑 Assertion Failed: 🛑" + actual + " !== " + expected);
+  }
+};
+
+assertEqual(1, 7);
