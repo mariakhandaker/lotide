c8305cea55ceb5db93de58a1bb4da9ae89aa071e
Maria Khandaker <mariakhandaker@gmail.com>
2020-06-23 01:56:30 +0000

    used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(" ✅ Assertion Passed ✅: " + actual + " === " + expected);
+    console.log(` ✅ Assertion Passed ✅:  ${actual} === ${expected} `);
   } else {
-    console.log(" 🛑 Assertion Failed: 🛑" + actual + " !== " + expected);
+    console.log(` 🛑 Assertion Failed: 🛑" ${actual} !== ${expected} `);
   }
 };
 
