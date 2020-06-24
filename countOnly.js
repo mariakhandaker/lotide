163e4b7670fbf7f2404a57f6e786ba69ff889969
Maria Khandaker <mariakhandaker@gmail.com>
2020-06-24 18:59:39 +0000

    countOnly function created for counting instances in an array if they exist in the input object

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅ Assertion Passed ✅:  ${actual} === ${expected} `);
+  } else {
+    console.log(` 🛑 Assertion Failed: 🛑" ${actual} !== ${expected} `);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }//iterate over everything in input array
+  // if it exists true in object, count instances in array
+  //    if no instances exist, no need to push
+  //     else push
+  // if false, no need to count
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
