e268680024a4b70ff111757e49ceda0a397a3c71
Maria <mariakhandaker@gmail.com>
2020-07-04 18:27:26 +0000

    are my commits working now? these are my lotide Mocha and Chai activities

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,3 @@
-/*
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-*/
-
 const assert = require('chai').assert;
 const head   = require('../head');
 
@@ -21,4 +11,3 @@ describe("#head", () => {
   });
 
 });
-*/
