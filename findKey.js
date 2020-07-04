9c3e23fb0a03a469f5cb0a9a7471055fb2e77fcc
Maria <mariakhandaker@gmail.com>
2020-07-04 21:29:31 +0000

    added tests using assertEqual

--- findKey.js
+++ findKey.js
@@ -7,14 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKey = (object, callback) => {
-  for (let key of Object.keys(object)) {
+  let keys = Object.keys(object);
+  for (let key of keys) {
+    //
     if (callback(object[key]) === true) {
       return key;
     }
   }
 };
 
-console.log(findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
@@ -22,4 +24,25 @@ console.log(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 
-}, x => x.stars === 2)); // => "noma"
+}, x => x.stars === 2), "noma");
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+
+}, x => x.stars > 5), undefined);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+
+}, x => x.stars >= 2), "Akaleri");
