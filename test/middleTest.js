0196f862dde295d3542362b075b46cac4ba1fe20
Maria <mariakhandaker@gmail.com>
2020-07-04 18:30:52 +0000

    middle and tail tests updated with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+describe("#middle", () => {
+  it("returns empty array for one or two values", () => {
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  
+  it("returns middle value when array is odd in length", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]) 
+  });
+
+  it("returns two middle value when array is even in length", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+
+});
